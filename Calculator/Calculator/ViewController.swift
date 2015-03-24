e0a13eee6c2c984163169995781f3e0e60e2c926
jedidja <me@jedidja.ca>
2015-03-24 11:32:12 -0400

    added number buttons and connected display

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -4,6 +4,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
     @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        println("digit = \(digit)")
     }
 }
