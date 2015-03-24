34a58e6ee5497aab072489bb8b3d8ff7b610d83d
jedidja <me@jedidja.ca>
2015-03-24 15:36:36 -0400

    simplify closure beyond belief

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
         }
         
         switch operation {
-        case "×": performOperation({ (op1, op2) in return op1 * op2 })
+        case "×": performOperation({ $0 * $1 })
             //        case "+":
             //        case "−":
             //        case "÷":
