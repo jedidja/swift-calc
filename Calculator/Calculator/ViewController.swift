3f6da0186121c183acbd232f83b41326da1eaab7
jedidja <me@jedidja.ca>
2015-03-25 04:27:58 -0400

    added cos

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -52,6 +52,7 @@ class ViewController: UIViewController {
         case "÷": performOperation { $1 / $0 }
         case "√": performOperation { sqrt($0) }
         case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
         default: break
         }
     }
